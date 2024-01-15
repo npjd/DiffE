362b0c443168d46f58f1894ea5d077468490da73
Nima Pourjafar <nima.pourjafar123@gmail.com>
2024-01-15 19:59:44 +0000

    switched to numpy format for data for consistency sake

--- custom_utils.py
+++ custom_utils.py
@@ -1,21 +1,34 @@
-from pandas import pd
+import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
+
+
 def load_data(root_dir, num_files):
-    df = pd.DataFrame()
+    # load the data into a dataframe
+    X = []
+    Y = []
+
     for i in range(num_files):
         text_file_path = root_dir + "/" + str(i) + "_word.txt"
         text_file = open(text_file_path, "r")
         emg_file_path = root_dir + "/" + str(i) + "_emg.npy"
         emg_file = np.load(emg_file_path)
+        X.append(emg_file)
+        Y.append(text_file.read())
 
-        df = df.append(pd.DataFrame({"emg": emg_file, "word": text_file.read()}))
+    # do one hot encoding on Y 
+    Y = np.array(Y)
+    Y = Y.reshape(-1, 1)
+    enc = OneHotEncoder(handle_unknown="ignore")
+    enc.fit(Y)
+    Y = enc.transform(Y).toarray()
+
+    max_dimension = max(arr.shape[0] for arr in X)
+    # pad the arrays to be the same size
+    X = np.array([np.pad(arr, ((0, max_dimension - arr.shape[0]), (0, 0)), 'constant') for arr in X])
     
-    # create a new column with the one-hot encoded labels for text
-    enc = OneHotEncoder(handle_unknown='ignore')
-    enc.fit(df[["word"]])
-    one_hot = enc.transform(df[["word"]]).toarray()
-    df["one_hot"] = list(one_hot)
-    
-    return df
+    return X,Y
+
+
+load_data("single_word_mouthed", 150)
